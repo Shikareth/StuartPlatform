50f74a5a6c548e4174bedd00a395f4411180f9cd
Shikareth <gothic344@gmail.com>
2018-07-15 19:17:28 +0200

    update

--- Tools/Math/Matrix.cs
+++ Tools/Math/Matrix.cs
@@ -867,5 +867,203 @@ namespace Tools.Math
             Matrix E { get; set; }
             Matrix V { get; set; }
         }
+
+        /*
+        C++ source code
+
+        fp_t pythag(fp_t a, fp_t b)
+        {
+          float absa,absb;
+          absa=fabs(a);
+          absb=fabs(b);
+          if(absa>absb)
+            return absa*sqrt(1.0+sqr(absb/absa));
+          else
+            return(absb==0.0)?0.0:absb*sqrt(1.0+sqr(absa/absb));
+        }
+
+        void svdcmp(fp_t **a, int m, int n, fp_t *w, fp_t **v)
+        {
+          fp_t g,scale,anorm;
+          fp_t *rv1=new fp_t [n]-1;
+          g=scale=anorm=0.0;
+          for(int i=1;i<=n;++i){
+            int l=i+1;
+            rv1[i]=scale*g;
+            g=scale=0.0;
+            if(i<=m){
+              for(int k=i;k<=m;++k) scale+=fabs(a[k][i]);
+              if(scale){
+                fp_t s=0.0;
+                for(int k=i;k<=m;++k){
+                  a[k][i]/=scale;
+                  s+=a[k][i]*a[k][i];
+                }
+                fp_t f=a[i][i];
+                g=-sign(sqrt(s),f);
+                fp_t h=f*g-s;
+                a[i][i]=f-g;
+                for(int j=l;j<=n;++j){
+                  fp_t sum=0.0;
+                  for(int k=i;k<=m;++k) sum+=a[k][i]*a[k][j];
+                  fp_t fct=sum/h;
+                  for(int k=i;k<=m;++k) a[k][j]+=fct*a[k][i];
+                }
+                for(int k=i;k<=m;++k) a[k][i]*=scale;
+              }
+            }
+            w[i]=scale*g;
+            g=scale=0.0;
+            if((i<=m)&&(i!=n)){
+              for(int k=l;k<=n;++k) scale+=fabs(a[i][k]);
+              if(scale){
+                fp_t s=0.0;
+                for(int k=l;k<=n;++k){
+                  a[i][k]/=scale;
+                  s+=a[i][k]*a[i][k];
+                }
+                fp_t f=a[i][l];
+                g=-sign(sqrt(s),f);
+                fp_t h=f*g-s;
+                a[i][l]=f-g;
+                for(int k=l;k<=n;++k) rv1[k]=a[i][k]/h;
+                for(int j=l;j<=m;++j){
+                  fp_t sum=0.0;
+                  for(int k=l;k<=n;++k) sum+=a[j][k]*a[i][k];
+                  for(int k=l;k<=n;++k) a[j][k]+=sum*rv1[k];
+                }
+                for(int k=l;k<=n;++k) a[i][k]*=scale;
+              }
+            }
+            anorm=max(anorm,(fabs(w[i])+fabs(rv1[i])));
+          }
+          {
+            fp_t f;
+            for(int i=n,l;i>=1;--i){
+              if(i<n){       // this makes f and l not dependent
+                if(f){
+                  for(int j=l;j<=n;++j) v[j][i]=(a[i][j]/a[i][l])/f;
+                    for(int j=l;j<=n;++j){
+                    fp_t sum=0.0;
+                    for(int k=l;k<=n;++k) sum+=a[i][k]*v[k][j];
+                    for(int k=l;k<=n;++k) v[k][j]+=sum*v[k][i];
+                  }
+                }
+                for(int j=l;j<=n;++j) v[i][j]=v[j][i]=0.0;
+              }
+              v[i][i]=1.0;
+              f=rv1[i];
+              l=i;
+            }
+          }
+          for(int i=min(m,n);i>=1;--i){
+            int l=i+1;
+            g=w[i];
+            for(int j=l;j<=n;++j) a[i][j]=0.0;
+            if(g){
+              g=1.0/g;
+              for(int j=l;j<=n;++j){
+                fp_t sum=0.0;
+                for(int k=l;k<=m;++k) sum+=a[k][i]*a[k][j];
+                fp_t f=(sum/a[i][i])*g;
+                for(int k=i;k<=m;++k) a[k][j]+=f*a[k][i];
+              }
+              for(int j=i;j<=m;++j) a[j][i]*=g;
+            }else for(int j=i;j<=m;++j) a[j][i]=0.0;
+            ++a[i][i];
+          }
+          for(int k=n;k>=1;--k){
+            for(int its=1;its<=30;++its){
+              int flag=1,nm,l;
+              for(l=k;l>=1;--l){
+                nm=l-1;
+                if((fp_t)(fabs(rv1[l])+anorm)==anorm){
+                  flag=0;
+                  break;
+                }
+                if((fp_t)(fabs(w[nm])+anorm)==anorm) break;
+              }
+              if(flag){
+                fp_t c=0.0,s=1.0;
+                for(int i=l;i<=k;++i){
+                  fp_t f=s*rv1[i];
+                  rv1[i]=c*rv1[i];
+                  if((fp_t)(fabs(f)+anorm)==anorm) break;
+                  g=w[i];
+                  fp_t h=pythag(f,g);
+                  w[i]=h;
+                  h=1.0/h;
+                  c=g*h;
+                  s=-f*h;
+                  for(int j=1;j<=m;++j){
+                    fp_t y=a[j][nm];
+                    fp_t z=a[j][i];
+                    a[j][nm]=y*c+z*s;
+                    a[j][i]=z*c-y*s;
+                  }
+                }
+              }
+              fp_t z=w[k];
+              if(l==k){
+                if(z<0.0){
+                  w[k]=-z;
+                  for(int j=1;j<=n;++j) v[j][k]=-v[j][k];
+                }
+                break;
+              }
+              if(its==30) exit(fprintf(stderr,"no convergence in 30 svdcmp iterations"));
+              fp_t x=w[l];
+              fp_t y=w[nm=k-1];
+              g=rv1[nm];
+              fp_t h=rv1[k];
+              fp_t f=((y-z)*(y+z)+(g-h)*(g+h))/(2.0*h*y);
+              g=pythag(f,1.0);
+              f=((x-z)*(x+z)+h*((y/(f+sign(g,f)))-h))/x;
+              fp_t c=1.0,s=1.0;
+              for(int j=l;j<=nm;++j){
+                int i=j+1;
+                g=rv1[i];
+                y=w[i];
+                h=s*g;
+                g*=c;
+                z=pythag(f,h);
+                rv1[j]=z;
+                c=f/z;
+                s=h/z;
+                f=x*c+g*s;
+                g=g*c-x*s;
+                h=y*s;
+                y*=c;
+                for(int jj=1;jj<=n;++jj){
+                  x=v[jj][j];
+                  z=v[jj][i];
+                  v[jj][j]=x*c+z*s;
+                  v[jj][i]=z*c-x*s;
+                }
+                z=pythag(f,h);
+                w[j]=z;
+                if(z){
+                  z=1.0/z;
+                  c=f*z;
+                  s=h*z;
+                }
+                f=c*g+s*y;
+                x=c*y-s*g;
+                for(int jj=1;jj<=m;++jj){
+                  y=a[jj][j];
+                  z=a[jj][i];
+                  a[jj][j]=y*c+z*s;
+                  a[jj][i]=z*c-y*s;
+                }
+              }
+              rv1[l]=0.0;
+              rv1[k]=f;
+              w[k]=x;
+            }
+          }
+          delete (rv1+1);
+        }
+ 
+        */
     }
 }
--- Tools/Math/Misc.cs
+++ Tools/Math/Misc.cs
@@ -57,7 +57,121 @@ namespace Tools.Math
             return To_min + ((To_max - To_min) / (From_max - From_min)) * (value - From_min);
         }
 
+        public static double[] FindPolynomialRoot_BrentDekker(double a, double b, double[] function, double epsilon = double.Epsilon)
+        {
+            int rank = function.Length - 1;
+            double[] roots = new double[rank];
+            double s = double.NaN;
+            double f_s = double.NaN;
+            double d = double.NaN;
 
+            // calculate f(a) and calculate f(b)
+            double f_a = CalculatePolynomialValue(a, function);
+            double f_b = CalculatePolynomialValue(b, function);
 
+            // exit function because the root is not bracketed
+            if (f_a * f_b >= 0) return null;
+            if(System.Math.Abs(f_a) < System.Math.Abs(f_b))
+            {
+                double t = a;
+                a = b;
+                b = t;
+
+                double f_t = f_a;
+                f_a = f_b;
+                f_b = f_t;
+            }
+
+            bool mflag = true;
+            double c = a;
+            double f_c;
+
+            do
+            {
+               
+                f_c = CalculatePolynomialValue(c, function);
+
+                if (f_a != f_c && f_b != f_c)
+                {
+                    s = (a * f_c - b * f_c) / ((f_a - f_b) * (f_a - f_c)) +
+                        (b * f_a - b * f_c) / ((f_b - f_a) * (f_b - f_c)) +
+                        (c * f_a - b * f_b) / ((f_c - f_a) * (f_c - f_b));
+                }
+                else
+                {
+                    s = b - f_b * (b - a) / (f_b - f_a);
+                }
+
+                if (!InRange((3 * a + b) / 4, b, s) ||
+                    (mflag && System.Math.Abs(s - b) >= System.Math.Abs(b - c) / 2) ||
+                    (!mflag && System.Math.Abs(s - b) >= System.Math.Abs(c - d) / 2) ||
+                    (mflag && System.Math.Abs(b - c) >= epsilon) ||
+                    (!mflag && System.Math.Abs(c - d) >= epsilon))
+                {
+                    s = (a + b) / 2;
+                    mflag = true;
+                }
+                else
+                    mflag = false;
+
+                f_s = CalculatePolynomialValue(s, function);
+                d = c;
+                c = b;
+
+                if (f_a * f_s < 0)
+                {
+                    b = s;
+                    f_b = f_s;
+                }
+                else
+                {
+                    a = s;
+                    f_a = f_s;
+                }
+
+                if (System.Math.Abs(f_a) < System.Math.Abs(f_b))
+                {
+                    double t = a;
+                    a = b;
+                    b = t;
+
+                    double f_t = f_a;
+                    f_a = f_b;
+                    f_b = f_t;
+                }
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(string.Format("[{0:N3} : {1:N3}]", s, b));
+
+                // repeat until f(b or s) = 0 or |b − a| is small enough (convergence)
+            } while (f_s != 0 && f_b != 0 && System.Math.Abs(b - a) >= epsilon);
+
+            roots[0] = s;
+            roots[1] = b;
+
+            return roots;
+        }
+
+        public static double CalculatePolynomialValue(double value, double[] function)
+        {
+            double result = 0;
+
+            for (int n = 0; n < function.Length; n++)
+                result += function[function.Length - 1 - n] * System.Math.Pow(value, n);
+
+            return result;
+        }
+
+        public static bool InRange(double a, double b, double value)
+        {
+            double lower = a;
+            double upper = b;
+            if(a > b)
+            {
+                lower = b;
+                upper = a;
+            }
+
+            return (value >= lower && value <= upper);
+        }
     }
 }
